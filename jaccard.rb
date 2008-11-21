afbbc945a849d39b3ecc21022bb43628ea1a8160
Daniel Haran <chebuctonian@mgmail.com>
2008-11-21 09:35:09 -0500

    adding recommendations_for(user_id)

--- jaccard.rb
+++ jaccard.rb
@@ -4,9 +4,36 @@ class Jaccard
   attr_accessor :similarities
   
   def initialize(user_hash)
-    @similarities = SimilarityMatrix.new(user_hash) do |a,b| 
+    @user_hash = user_hash
+    @similarities = SimilarityMatrix.new(@user_hash) do |a,b| 
       Jaccard.coefficient(a,b)
     end
+  end
+  
+  def recommendations_for(user_key)
+    recos, total_similarity = {}, 0
+    
+    (@user_hash[user_key] - [user_key]).each do |following|
+      next if @user_hash[following].nil?
+      similarity = @similarities.find(user_key,following)
+      total_similarity += similarity
+      @user_hash[following].each do |recommendation|
+        if recos.has_key?(recommendation)
+          recos[recommendation] += similarity
+        else
+          recos[recommendation] = similarity
+        end
+      end
+    end
+    
+    return {} if total_similarity == 0
+    
+    recos.delete_if {|k,v| @user_hash[user_key].include?(k)}
+    recos.keys.each do |key|
+      recos[key] /= total_similarity #normalize data to max 1
+    end
+    
+    recos
   end
   
   def self.coefficient(a,b)
--- jaccard_test.rb
+++ jaccard_test.rb
@@ -1,11 +1,16 @@
 require 'test/unit'
 require 'jaccard'
+require 'rubygems'
+require 'activesupport'
 
 class JaccardTest < Test::Unit::TestCase
   def setup
     @jaccard = Jaccard.new({
-      '330' => ["250", "276", "330", "724", "739"], 
-      '250' =>["213", "228", "250", "276", "290", "326", "330", "489", "531", "572", "579", "688", "724", "739"]})
+      "250" => ["213", "228", "250", "276", "290", "326", "330", "489", "531", "572", "579", "688", "724", "739"],
+      "276" => ["228", "250", "276", "326"],
+      "330" => ["250", "276", "330", "724", "739"],
+      "724" => ["60", "149", "177", "191", "213", "228", "250", "262", "326", "336", "572", "579", "688", "697", "724", "739", "783", "849", "860", "968", "997", "1827", "1914", "2008", "3143"],
+      "739" => ["213", "228", "250", "330", "724", "739", "860", "3509", "16987", "16990"]})
   end
   
   def test_initialize
@@ -15,7 +20,31 @@ class JaccardTest < Test::Unit::TestCase
   def test_similarity
     assert_equal 0.357, @jaccard.similarities.find("250", "330")
   end
-
+  
+  def test_recommendations
+    recos = @jaccard.recommendations_for("330")
+    assert_not_nil recos
+    #puts recos.to_a.sort_by(&:last).reverse.inspect
+    assert recos.keys.include?("228") # followed by all other users
+    assert recos["228"] > recos["60"], "user 60 is only followed by 1 person, so should be lower than 228"
+  end
+  
+  def test_recommendation_should_be_empty_if_absolutely_no_overlap
+    jaccard = Jaccard.new({
+      "1" => ["2", "3"],
+      "4" => ["5", "6"]})
+    assert_equal ({}), jaccard.recommendations_for("1")
+  end
+    
+  def test_normalizes_from_0_to_1
+    recos = @jaccard.recommendations_for("330")
+    assert !recos.has_key?("250"), "should not recommend someone you're already subscribed to"
+    
+    assert recos.values.max <= 1, "values should be normalized"
+    
+    assert_equal 1.0, recos["228"], "everyone else follows 228, so it should return 1"
+  end
+  
   def test_coefficient_output
     {
       [[1,2,3],[1,2,3]] => 1,
